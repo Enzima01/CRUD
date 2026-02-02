887dd031b2e67403ff265b8a8a29549184f91d8e
Enzima01 <enzo.h.favaro@gmail.com>
2026-02-02 18:31:55 -0300

    CRUD FINISHED!

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.List;
 import java.util.Scanner;
 
 import model.dao.DaoFactory;
@@ -9,7 +10,8 @@ import model.entities.User;
 public class Program {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int option =0;
+
+		int option = 0;
 		UserDao userDao = DaoFactory.createUserDao();
 
 		do {
@@ -27,31 +29,67 @@ public class Program {
 
 			switch (option) {
 			case 1:
-				System.out.println("\n=== REGISTER USER ===\n");
+				System.out.println("\n=== REGISTER USER ===");
 				System.out.print("User name: ");
 				String name = sc.nextLine();
 				System.out.print("User email: ");
 				String email = sc.nextLine();
 				User user = new User(null, name, email);
 				userDao.insert(user);
-				System.out.println("\nInserted! New id = " + user.getId() + " | " + user.getName() + " | " + user.getEmail() + "\n");
+				System.out.println("\nNew user inserted!: " + user.getId() + " | " + user.getName() + " | "
+						+ user.getEmail() + "\n");
 				break;
 			case 2:
-				System.out.println("\n=== EDIT USER ===\n");
+				System.out.println("\n=== EDIT USER ===");
+				System.out.print("Enter user id to edit data: ");
+				int id = sc.nextInt();
+				sc.nextLine();
+
+				User existingUser = userDao.findById(id);
+
+				if (existingUser == null) {
+					System.out.println("\nUser not found!\n");
+					break;
+				}
+
+				System.out.print("Enter new user name (Leave blank if you don't want to edit!): ");
+				name = sc.nextLine();
+				System.out.print("Enter new user email (Leave blank if you don't want to edit!): ");
+				email = sc.nextLine();
+				String newName = name.isEmpty() ? existingUser.getName() : name;
+				String newEmail = email.isEmpty() ? existingUser.getEmail() : email;
+				user = new User(id, newName, newEmail);
+				userDao.update(user);
+				System.out.println(
+						"\nUser Edited!: " + user.getId() + " | " + user.getName() + " | " + user.getEmail() + "\n");
 				break;
 			case 3:
-				System.out.println("\n=== LIST USERS ===\n");
+				System.out.println("\n=== LIST USERS ===");
+				List<User> list = userDao.findAll();
+				for (User obj : list) {
+					System.out.println(obj);
+				}
+				System.out.println();
 				break;
 			case 4:
-				System.out.println("\n=== SEACH USER ===\n");
+				System.out.println("\n=== SEARCH USER ===");
+				System.out.print("Enter user id: ");
+				id = sc.nextInt();
+				user = userDao.findById(id);
+				System.out.println(user + "\n");
 				break;
 			case 5:
 				System.out.println("\n=== DELETE USER ===\n");
+				System.out.print("Enter user id to delete: ");
+				id = sc.nextInt();
+				userDao.deleteById(id);
+				System.out.println("User Deleted!" + "\n");
 				break;
 			case 6:
 				System.out.println("\nEXITING...\n");
 				break;
 			}
 		} while (option != 6);
+		sc.close();
 	}
 }
--- src/model/dao/impl/UserDaoJDBC.java
+++ src/model/dao/impl/UserDaoJDBC.java
@@ -5,7 +5,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -49,24 +52,94 @@ public class UserDaoJDBC implements UserDao {
 
 	@Override
 	public void update(User u) {
-
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("UPDATE user SET name = ?, email = ? WHERE id = ?");
+			st.setString(1, u.getName());
+			st.setString(2, u.getEmail());
+			st.setInt(3, u.getId());
+			st.executeUpdate();
+			System.out.println("User updated!");
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
 	public void deleteById(Integer id) {
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("DELETE FROM user WHERE id = ?");
+			st.setInt(1, id);
+			int rowsAffected = st.executeUpdate();
+			if (rowsAffected == 0) {
+				throw new DbException("User not found!");
+			}
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
 
+		}
 	}
 
 	@Override
 	public User findById(Integer id) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement("SELECT * FROM user WHERE id = ?");
+			st.setInt(1, id);
+			rs = st.executeQuery();
+			if (rs.next()) {
+				User user = instantiateUser(rs);
+				return user;
+			} else {
+				throw new DbException("User not found!");
+			}
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 
-		return null;
 	}
 
 	@Override
 	public List<User> findAll() {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement("SELECT * FROM user");
+			rs = st.executeQuery();
+			List<User> list = new ArrayList<>();
+			Map<Integer, User> map = new HashMap<>();
+			while (rs.next()) {
+				User user = map.get(rs.getInt("id"));
+				if (user == null) {
+					user = instantiateUser(rs);
+					map.put(rs.getInt("id"), user);
+				}
+				list.add(user);
+			}
+			return list;
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+	}
 
-		return null;
+	private User instantiateUser(ResultSet rs) throws SQLException {
+		User user = new User();
+		user.setId(rs.getInt("id"));
+		user.setName(rs.getString("name"));
+		user.setEmail(rs.getString("email"));
+		return user;
 	}
 
 }
